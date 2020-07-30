2cb9f1406bb80eac32ceeef354067f948303c7a0
Munishtutoo6 <Munishtutoo6@gmail.com>
2020-07-30 22:48:06 +0530

    Updates

---
+++ precache-manifest.29ab695f36cce8132e41c7175c3e272e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23002e1a8fcf11842a1edaba7d32ec93",
+    "url": "/Munish-Project/index.html"
+  },
+  {
+    "revision": "ab88ac1a77c2864cbf7e",
+    "url": "/Munish-Project/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1fc692b101a0920c3ad1",
+    "url": "/Munish-Project/static/css/main.b0a9398d.chunk.css"
+  },
+  {
+    "revision": "ab88ac1a77c2864cbf7e",
+    "url": "/Munish-Project/static/js/2.3f72f909.chunk.js"
+  },
+  {
+    "revision": "1fc692b101a0920c3ad1",
+    "url": "/Munish-Project/static/js/main.aa09b79a.chunk.js"
+  },
+  {
+    "revision": "d68597d154a9c3182af7",
+    "url": "/Munish-Project/static/js/runtime~main.1aa0afcf.js"
+  },
+  {
+    "revision": "17106380b6a2f5edad4f03b05b34f1c5",
+    "url": "/Munish-Project/static/media/Picture.17106380.jpg"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/Munish-Project/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "2cb23828e9901c7acce02a72f5122d8b",
+    "url": "/Munish-Project/static/media/myImage.2cb23828.png"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/Munish-Project/static/media/todolist.ac82d97d.png"
+  }
+]);
